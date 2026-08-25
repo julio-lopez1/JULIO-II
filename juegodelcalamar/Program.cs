f5cc69dab7f87d13e9b1a9573131524cbbd3c372
julio-lopez1 <jl2503143@gmail.com>
2026-08-25 12:11:28 -0400

    init
    commi

---
+++ juegodelcalamar/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace juegodelcalamar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
